362667ae40d3335cee8939c9c274a54fcd3b6013
Rakibmahmud <hasansaikat74@gmail.com>
2024-05-23 09:44:33 +0600

    added mui intregation

---
+++ src/lib/providers/Providers.tsx
@@ -0,0 +1,7 @@
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "../theme/theme";
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+export default Providers;
